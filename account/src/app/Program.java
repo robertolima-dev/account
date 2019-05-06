225ca5627c545eba71f393da9db1b6f107e71724
Roberto Lima <robertolima@MacBook-Pro-de-Roberto.local>
2019-05-06 11:01:08 -0300

    v1.0.1

--- account/src/app/Program.java
+++ account/src/app/Program.java
@@ -32,5 +32,12 @@ public class Program {
 			System.out.println("Update!");
 		}
 
+		Account acc6 = new Account(1005, "Mary", 1000.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+		
+		Account acc7 = new SavingsAccount(1006, "Melody", 1000.0, 0.01);
+		acc7.withdraw(200.00);
+		System.out.println(acc7.getBalance());
 	}
 }
--- account/src/entities/SavingsAccount.java
+++ account/src/entities/SavingsAccount.java
@@ -26,4 +26,9 @@ public class SavingsAccount extends Account {
 	public void updateBalance() {
 		balance += balance * interestRate;
 	}
+	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
 }
