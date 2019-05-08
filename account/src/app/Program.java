91308057fb50e4fe5548a5980847ad7f1152f1b1
Roberto Lima <robertolima@MacBook-Pro-de-Roberto.local>
2019-05-08 11:01:51 -0300

    v1.0.5

--- account/src/app/Program.java
+++ account/src/app/Program.java
@@ -1,8 +1,5 @@
 package app;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -11,22 +8,49 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Account acc = new Account(1001, "Alex", 0.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "John", 0.0, 500.0);
 
+		// UPCASTING
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Carl", 0.0, 300.0);
+		Account acc3 = new SavingsAccount(1004, "Morgan", 0.0, 0.01);
 
-		List<Account> list = new ArrayList<>();
-
-		list.add(new SavingsAccount(1025, "Carl", 1000.0, 0.01));
-		list.add(new SavingsAccount(1028, "Madalene", 1000.0, 0.01));
-		list.add(new BusinessAccount(1030, "Robert", 1000.0, 300.0));
-		list.add(new BusinessAccount(1032, "Brad", 1000.0, 500.0));
-
-		double sum = 0.0;
-		for (Account acc10 : list) {
-			sum += acc10.getBalance();
+		// DOWNCASTING
+		BusinessAccount acc4 = (BusinessAccount) acc2;
+		acc4.loan(100.0);
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			// System.out.println("Loan!");
 		}
 
-		System.out.printf("Total balance: %.2f%n", sum);
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			// System.out.println("Update!");
+		}
 
+		Account acc6 = new Account(1005, "Mary", 1000.0);
+		acc6.withdraw(200.0);
+		// System.out.println(acc6.getBalance());
+		
+		Account acc7 = new SavingsAccount(1006, "Melody", 1000.0, 0.01);
+		acc7.withdraw(200.00);
+		// System.out.println(acc7.getBalance());
+		
+		Account acc8 = new BusinessAccount(1007, "Tedy", 1000.0, 500.00);
+		acc8.withdraw(200.00);
+		// System.out.println(acc8.getBalance());
+		
+		Account x = new Account(1010, "David", 1000.0);
+		Account y = new SavingsAccount(1020, "Joane", 1000.0, 0.01);
+		
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 	}
 }
--- account/src/entities/Account.java
+++ account/src/entities/Account.java
@@ -1,6 +1,6 @@
 package entities;
 
-public abstract class Account {
+public class Account {
 
 	private Integer number;
 	private String holder;
